d76526336db32a48de253addf678bca04a4caf2b
Mossati <xmossati9786@gmail.com>
2023-09-15 10:18:39 +0200

    code

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
-const images = ["/assets/kaarten/Kaart_Mohammed.png"];
 
-for (let x = 0; x < images.length; x++) {            
-    document.getElementById("img").src = "/assets/kaarten/Kaart_Mohammed.png";
+function loadImages(){
+    const images = ['assets/kaarten/Kaart_Mohammed.png', 'assets/kaarten/Kaart_Kaan.png', 'assets/kaarten/Kaart_Rukiya.png'];
+
+    const container = document.getElementById('image-container');
+     
+    for (let i = 0; i < images.length; i++) {
+        const div = document.createElement('div');
+        div.classList.add("card");
+        const img = document.createElement('img');
+        img.src = images[i];
+        img.width = 250;
+        img.height = 200;
+        container.appendChild(div);
+        div.appendChild(img);
+    }
 }
