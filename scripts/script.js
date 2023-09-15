ace045f95b4f839da672ccd3768b2adf5c1eb9c9
Mossati <xmossati9786@gmail.com>
2023-09-15 11:39:26 +0200

    code

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,22 @@
 function loadImages(){
     const images = ['assets/kaarten/Kaart_Mohammed.png', 'assets/kaarten/Kaart_Kaan.png', 'assets/kaarten/Kaart_Rukiya.png'];
 
+    const links = ['https://mossati.github.io/your-tribe-profile-card/', 'https://khdulkadir.github.io/your-tribe-profile-card/','https://rukiyatossou.github.io/your-tribe-profile-card/'];
+
     const container = document.getElementById('image-container');
      
     for (let i = 0; i < images.length; i++) {
         const div = document.createElement('div');
         div.classList.add("card");
+        const a = document.createElement('a');
         const img = document.createElement('img');
+        a.href = links[i]
         img.src = images[i];
         img.width = 250;
         img.height = 200;
         container.appendChild(div);
-        div.appendChild(img);
+        div.appendChild(a);
+        a.appendChild(img);
     }
 }
+
