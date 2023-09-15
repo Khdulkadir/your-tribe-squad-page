e42ca6f6907ab2e8b6a8ea42cbd03e782186d560
Mossati <xmossati9786@gmail.com>
2023-09-15 09:14:50 +0200

    code

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+const images = ["/assets/kaarten/Kaart_Mohammed.png"];
+
+for (let x = 0; x < images.length; x++) {            
+    document.getElementById("img").src = "/assets/kaarten/Kaart_Mohammed.png";
+}
